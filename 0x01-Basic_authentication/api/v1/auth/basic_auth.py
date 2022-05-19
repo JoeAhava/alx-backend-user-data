369b947f889cb2e9d594f7812ef9a3bbb24d3b31
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-19 17:09:49 +0300

    BasicAuth extract encoded str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Auth module
 """
 
+from base64 import b64decode, decode
 from re import split
 from api.v1.auth.auth import Auth
 
@@ -29,14 +30,30 @@ class BasicAuth(Auth):
             return None
         return split(" ", authorization_header)[1]
 
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        decode base64
+        """
+        if (base64_authorization_header is None) or (
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            result = b64decode(base64_authorization_header).decode('utf-8')
+            print(result)
+            return result
+        except Exception as e:
+            print(e)
+            return None
+
 
 if __name__ == "__main__":
     a = BasicAuth()
-    print(a.extract_base64_authorization_header(None))
-    print(a.extract_base64_authorization_header(89))
-    print(a.extract_base64_authorization_header("Holberton School"))
-    print(a.extract_base64_authorization_header("Basic Holberton"))
-    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
-    print(a.extract_base64_authorization_header(
-        "Basic SG9sYmVydG9uIFNjaG9vbA=="))
+    print(a.decode_base64_authorization_header(None))
+    print(a.decode_base64_authorization_header(89))
+    print(a.decode_base64_authorization_header("Holberton School"))
+    print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+    print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
+    print(a.decode_base64_authorization_header(
+        a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
     print(a.extract_base64_authorization_header("Basic1234"))
