62ae46588b188a29eb0f1e74b762073dfaad8507
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-19 16:43:24 +0300

    BasicAuth extract encoded str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 Basic Auth
 """
 
-from api.v1.auth.auth import Auth
+from re import split
+from auth import Auth
 
 
 class BasicAuth(Auth):
@@ -13,3 +14,23 @@ class BasicAuth(Auth):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extract string
+        """
+        if authorization_header is None or not isinstance(authorization_header, str) or split(" ", authorization_header)[0] != 'Basic':
+            return None
+        return split(" ", authorization_header)[1]
+
+
+if __name__ == "__main__":
+    a = BasicAuth()
+    print(a.extract_base64_authorization_header(None))
+    print(a.extract_base64_authorization_header(89))
+    print(a.extract_base64_authorization_header("Holberton School"))
+    print(a.extract_base64_authorization_header("Basic Holberton"))
+    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+    print(a.extract_base64_authorization_header(
+        "Basic SG9sYmVydG9uIFNjaG9vbA=="))
+    print(a.extract_base64_authorization_header("Basic1234"))
