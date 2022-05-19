3ba12efbf49373ca210817edf62753c7616edbf6
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-19 14:27:27 +0300

    require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ auth handler
 '''
 
 
+from operator import eq
 from typing import List, TypeVar
 from flask import request
 
@@ -17,7 +18,15 @@ class Auth:
         '''
         require_auth
         '''
-        return False
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        temp_path = path
+        if temp_path[-1] != '/':
+            temp_path += '/'
+
+        if temp_path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''
@@ -30,3 +39,15 @@ class Auth:
         current_user
         '''
         return None
+
+
+if __name__ == '__main__':
+    a = Auth()
+    print(a.require_auth(None, None))
+    print(a.require_auth(None, []))
+    print(a.require_auth("/api/v1/status/", []))
+    print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
+    print(a.require_auth("/api/v1/users",
+          ["/api/v1/status/", "/api/v1/stats"]))
