a1929dea48e8dc5ddba3ce66e5c35a2e0653a442
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-19 17:24:24 +0300

    BasicAuth extract uname and p str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,9 @@ Basic Auth module
 """
 
 from base64 import b64decode, decode
+from operator import contains
 from re import split
+from typing import Tuple
 from api.v1.auth.auth import Auth
 
 
@@ -46,16 +48,29 @@ class BasicAuth(Auth):
             # print(e)
             return None
 
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+    ) -> Tuple[str, str]:
+        """Extract username
+        and password
+        """
+        if (decoded_base64_authorization_header is None) or (
+            not isinstance(
+                decoded_base64_authorization_header, str)
+        ) or (
+            ':' not in decoded_base64_authorization_header
+        ):
+            return None
+        return (
+            split(":", decoded_base64_authorization_header)[0],
+            split(":", decoded_base64_authorization_header)[1])
+
 
 if __name__ == "__main__":
     a = BasicAuth()
-    print(a.decode_base64_authorization_header(
-        "NopBase64"))
-    print(a.decode_base64_authorization_header(89))
-    print(a.decode_base64_authorization_header("Holberton School"))
-    print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
-    print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
-    print(a.decode_base64_authorization_header(
-        a.extract_base64_authorization_header(
-            "Basic SG9sYmVydG9uIFNjaG9vbA==")))
-    print(a.extract_base64_authorization_header("Basic1234"))
+    print(a.extract_user_credentials(None))
+    print(a.extract_user_credentials(89))
+    print(a.extract_user_credentials("Holberton School"))
+    print(a.extract_user_credentials("Holberton:School"))
+    print(a.extract_user_credentials("bob@gmail.com:toto1234"))
