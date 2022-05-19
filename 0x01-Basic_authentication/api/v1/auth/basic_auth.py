69b9f10fc03097b7d1592c8cc7b0c181a40ebd20
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-19 17:17:12 +0300

    BasicAuth extract encoded str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,5 +56,6 @@ if __name__ == "__main__":
     print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
     print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
     print(a.decode_base64_authorization_header(
-        a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
+        a.extract_base64_authorization_header(
+            "Basic SG9sYmVydG9uIFNjaG9vbA==")))
     print(a.extract_base64_authorization_header("Basic1234"))
