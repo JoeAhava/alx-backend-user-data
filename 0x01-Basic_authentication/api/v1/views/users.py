d0a0dc28b2d0ac98c0e7ff517f5a33f8ee06e9e3
Yohannes Terfa <lightshadow011@gmail.com>
2022-05-23 11:39:59 +0300

    session fix

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" Module of Users views
-"""
+'''
+Module of Users views
+'''
 
 
 from api.v1.views import app_views
@@ -10,23 +11,23 @@ from models.user import User
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
-    """ GET /api/v1/users
+    ''' GET /api/v1/users
     Return:
       - list of all User objects JSON represented
-    """
+    '''
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
-    """ GET /api/v1/users/:id
+    ''' GET /api/v1/users/:id
     Path parameter:
       - User ID
     Return:
       - User object JSON represented
       - 404 if the User ID doesn't exist
-    """
+    '''
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -37,13 +38,13 @@ def view_one_user(user_id: str = None) -> str:
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
-    """ DELETE /api/v1/users/:id
+    ''' DELETE /api/v1/users/:id
     Path parameter:
       - User ID
     Return:
       - empty JSON is the User has been correctly deleted
       - 404 if the User ID doesn't exist
-    """
+    '''
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -55,7 +56,7 @@ def delete_user(user_id: str = None) -> str:
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
-    """ POST /api/v1/users/
+    ''' POST /api/v1/users/
     JSON body:
       - email
       - password
@@ -64,7 +65,7 @@ def create_user() -> str:
     Return:
       - User object JSON represented
       - 400 if can't create the new User
-    """
+    '''
     rj = None
     error_msg = None
     try:
@@ -72,18 +73,18 @@ def create_user() -> str:
     except Exception as e:
         rj = None
     if rj is None:
-        error_msg = "Wrong format"
-    if error_msg is None and rj.get("email", "") == "":
-        error_msg = "email missing"
-    if error_msg is None and rj.get("password", "") == "":
-        error_msg = "password missing"
+        error_msg = 'Wrong format'
+    if error_msg is None and rj.get('email', '') == '':
+        error_msg = 'email missing'
+    if error_msg is None and rj.get('password', '') == '':
+        error_msg = 'password missing'
     if error_msg is None:
         try:
             user = User()
-            user.email = rj.get("email")
-            user.password = rj.get("password")
-            user.first_name = rj.get("first_name")
-            user.last_name = rj.get("last_name")
+            user.email = rj.get('email')
+            user.password = rj.get('password')
+            user.first_name = rj.get('first_name')
+            user.last_name = rj.get('last_name')
             user.save()
             return jsonify(user.to_json()), 201
         except Exception as e:
@@ -93,7 +94,7 @@ def create_user() -> str:
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
-    """ PUT /api/v1/users/:id
+    ''' PUT /api/v1/users/:id
     Path parameter:
       - User ID
     JSON body:
@@ -103,7 +104,7 @@ def update_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
       - 400 if can't update the User
-    """
+    '''
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -115,7 +116,7 @@ def update_user(user_id: str = None) -> str:
     except Exception as e:
         rj = None
     if rj is None:
-        return jsonify({'error': "Wrong format"}), 400
+        return jsonify({'error': 'Wrong format'}), 400
     if rj.get('first_name') is not None:
         user.first_name = rj.get('first_name')
     if rj.get('last_name') is not None:
